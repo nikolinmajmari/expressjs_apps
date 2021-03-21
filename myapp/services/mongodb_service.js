22bff38be544a409d5dfaf362a5f5792485b2ec0
nikolinmajmari <51003589+nikolinmajmari@users.noreply.github.com>
2021-03-22 00:51:24 +0100

    removed redunant files

--- myapp/services/mongodb_service.js
+++
@@ -1,9 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://root:Subset3036@cluster0.poljx.mongodb.net/ PROJECT 0?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log(collection);
-    client.close();
-});
-module.exports =  client;
